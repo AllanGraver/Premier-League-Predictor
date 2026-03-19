c581b33e3b049839cd346055383884c5cc414d1e
AllanGraver <allangraver@gmail.com>
2026-03-19 20:58:50 +0100

    Add more teams to TEAMNAME_MAP

--- src/teamname_map.py
+++ src/teamname_map.py
@@ -1,16 +1,26 @@
 TEAMNAME_MAP = {
     # football-data.org  -> Understat
-    "Spurs": "Tottenham Hotspur",
-    "Wolves": "Wolverhampton Wanderers",
-    "Man United": "Manchester United",
-    "Man City": "Manchester City",
-    "Nott'm Forest": "Nottingham Forest",
-    "Sheffield Utd": "Sheffield United",
-    "Sheff Utd": "Sheffield United",
-    "Brighton & Hove Albion": "Brighton",
-    "Newcastle": "Newcastle United",
-    "West Ham United": "West Ham",
-}
+    "Arsenal FC": "Arsenal",
+    "Brentford FC": "Brentford",
+    "Brighton & Hove Albion FC": "Brighton",
+    "Burnley FC": "Burnley",
+    "Chelsea FC": "Chelsea",
+    "Crystal Palace FC": "Crystal Palace",
+    "AFC Bournemouth": "Bournemouth",
+    "Aston Villa FC": "Aston Villa",
+    "Everton FC": "Everton",
+    "Fulham FC": "Fulham",
+    "Leeds United FC": "Leeds",
+    "Liverpool FC": "Liverpool",
+    "Manchester United FC": "Manchester United",
+    "Manchester City FC": "Manchester City",
+    "Newcastle United": "Newcastle United",
+    "Nottingham Forest FC": "Nottingham Forest",
+    "Sunderland AFC": "Sunderland",
+    "Tottenham Hotspur FC": "Tottenham",
+    "West Ham United FC": "West Ham",
+    "Wolverhampton Wanderers FC": "Wolverhampton Wanderers",
+    }
 
 def normalize_team(name: str) -> str:
     return TEAMNAME_MAP.get(name, name)
