6f2cb66bc757b5e2f9c6d8b470b526f0794168b8
AllanGraver <allangraver@gmail.com>
2026-03-19 18:35:55 +0100

    Update league name from 'EPL' to 'ENG-Premier League'

--- src/xg_understat.py
+++ src/xg_understat.py
@@ -12,7 +12,7 @@ def fetch_epl_schedule_with_xg() -> pd.DataFrame:
     date, home_team, away_team, home_xg, away_xg, status
     """
     # 1) Get available seasons first
-    us0 = sd.Understat(leagues="EPL")
+    us0 = sd.Understat(leagues="ENG-Premier League")
     seasons = us0.read_seasons()  # supported method [5](https://github.com/probberechts/soccerdata/blob/master/docs/reference/understat.rst)
 
     # Pick the latest season in the table (format depends on the source)
@@ -20,7 +20,7 @@ def fetch_epl_schedule_with_xg() -> pd.DataFrame:
     latest = seasons["season"].max()
 
     # 2) Fetch schedule for latest season (schedule includes xG) [3](https://deepwiki.com/probberechts/soccerdata/3.5-understat-and-sofascore-scrapers)[5](https://github.com/probberechts/soccerdata/blob/master/docs/reference/understat.rst)
-    us = sd.Understat(leagues="EPL", seasons=[latest])
+    us = sd.Understat(leagues="ENG-Premier League", seasons=[latest])
     sched = us.read_schedule()
 
     # Inspect columns once locally if needed: print(sched.columns)
